751ddfb4552034c8077d31f0ff13b2cf2dc7cae0
jhnferraris <contact@jhnferraris.dev>
2024-05-06 13:38:19 +0800

    Initial commit

---
+++ netlify/functions/api.ts
@@ -0,0 +1,24 @@
+import express, { Router } from "express";
+import serverless from "serverless-http";
+import bodyParser from "body-parser";
+
+const api = express();
+api.use(bodyParser.json())
+
+const router = Router();
+router.post("/webhook", (req, res) => {
+  processWebhookNotificationData(req.body);
+  res.send({
+    webhookDataProcessed: 'success',
+  })
+});
+
+const processWebhookNotificationData = (data) => {
+  // TODO: You will have to implement the logic of this method.
+  // i.e. DB updates, fulfillment flow, etc.
+  console.log(`Processing data`);
+  console.log(data);
+  console.log(`Done`)
+}
+
+export const handler = serverless(api);
